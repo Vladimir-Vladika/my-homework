304187b3f864d48605b3168ca97a073bf0aaa9cd
Vladimir-Vladika <vladimirsiraev911@gmail.com>
2025-09-25 07:45:48 +0700

    add new code
    Добавил новый код

---
+++ hw.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = input('+', '-', '/', '*')
+
+if c == '+':
+    print(a+b)
+elif c == '-':
+    print(a-b)
+elif c == '/':
+    print(a/b)
+elif c == '*':
+    print(a*b)
+else:
+    print('Вы ввели неправельную операцию')
